55f2ed24f7f9736b2faee49fda2d8e89554eded6
tcltere <t.canette@gmail.com>
2018-01-24 18:33:30 -0300

    subiendo readme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,3 @@
 $(document).ready(()=>{
-  $('#images').mouseover(() =>{
-    if ($('#images').hasClass('img-selected')) {
-      /* en el caso que exista ya una imagen seleccionada la eliminamos para que únicamente solo se tenga una imagen seleccionada*/
-      $('#images').removeClass('img-selected');
-      // añadimos la clase de la imagen seleccionada
-      $('#images').addClass('img-selected');
-    }
-    const alt = $('#images').attr('alt');
-    console.log(alt);
-  }); 
-});
+  
+})
