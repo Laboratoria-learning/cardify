f778e4b5d0208c423fd31a1a8a789f8291bc4bdf
AriellaRogueN <arrodriguezn@gmail.com>
2018-01-24 16:27:23 -0300

    Iniciando configuracion testeo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
 $(document).ready(()=>{
-
-/*hola
-
+  $('#images').mouseover(() =>{
+    if ($('#images').hasClass('img-selected')) {
+      /* en el caso que exista ya una imagen seleccionada la eliminamos para que únicamente solo se tenga una imagen seleccionada*/
+      $('#images').removeClass('img-selected');
+      // añadimos la clase de la imagen seleccionada
+      $('#images').addClass('img-selected');
+    }
+    const alt = $('#images').attr('alt');
+    console.log(alt);
+  });
 });
