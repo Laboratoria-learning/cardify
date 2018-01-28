b4914f30d58957154ffc1eccdaec8c7eb2788ad8
aurorex <selva_beat@hotmail.com>
2018-01-28 21:47:34 +0100

    modificando funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 $(document).ready(() => {
+  alert("jajaj");
   let containerA = $('.container');
   
   let pluginImage = (containerX) => {
     let searchImage = containerX.find('img');
     console.log(searchImage);
+    console.log("jasja")
 
     for (let i = 0; i < searchImage.length; i++) {
       console.log(searchImage[i]);
-      let figure = $('<figure style="color:transparent" >')
+      let figure = $('<figure style="color:transparent; margin:40px 0"></figure>');
+   
       figure.append(searchImage[i]);
       let altImage = $(searchImage[i]).attr('alt');
-      figure.append('<figcaption style="">' + altImage + '</figcaption>' );
+      figure.append('<figcaption class="figcaption-image" >' + altImage + '</figcaption>');
      
       containerX.append(figure);
 
-      figure.on('mouseover', () => {
-        $(searchImage[i]).attr('hidden', 'hidden');
-        $(figure).css({'color': 'black', 'width': '400px', 'height': '200px', 'text-align': 'center', 'line-height': '200px', 'font-size': '50px'});
-      });
+      figure.hover( 
+        () => {
+       
+          $(searchImage[i]).hide('slow'); 
+         
+          $(figure).css({'color': 'black', 'width': '150px','text-align': 'center' , 'font-size': '20px'});
+        }, () => {
+       
+          $(searchImage[i]).show('slow'); 
+         
+          $(figure).css({'color': 'transparent', 'width': '150px','text-align': 'center' , 'font-size': '20px'});
+        }
+      );
 
-      figure.on('mouseout', () => {
-        $(searchImage[i]).removeAttr('hidden');
-        $(figure).css({'color': 'white'});
-      });
+
     }
+    
   }
   pluginImage(containerA);
 });
