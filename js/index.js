2c42f1bb204d70fc10e25fce431180f7c4135892
Jennifecarmen <jennifercarmens@gmail.com>
2018-01-27 23:03:51 -0500

    funcionalidad images alt

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-
-
-
+var images = $('.container img');
+for (var i = 0;i < images.length;i++) {
+  $('.container').append(
+    '<figure>' +
+    '<img src='+images[i].src +' width="200px"> </img>'+
+    '<figcaption>'+images[i].alt +'</figcaption>' +
+    '</figure>'
+  );
+  $(images[i]).remove();
+}
