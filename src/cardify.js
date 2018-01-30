d2869b287da999ed8a94de7b5e889f7a51996834
JessicaMP <jessica95.mp@gmail.com>
2018-01-29 20:38:12 -0500

    Corrigiendo funcionalidad

--- src/cardify.js
+++ src/cardify.js
@@ -3,33 +3,35 @@ $(document).ready(() => {
 
   let pluginImage = (containerX) => {
     let searchImage = containerX.find('img');
-    searchImage.css({'width': '335px', 'height': '200px', 'border': '4px solid white', 'border-radius': '10px'});
-    console.log(searchImage);
 
+    console.log(searchImage);
 
     for (let i = 0; i < searchImage.length; i++) {
       console.log(searchImage[i]);
-      searchImage.css({'width': '335px', 'height': '200px',     'border-radius': '10px'});
+      searchImage.css({'width': '335px', 'height': '200px','border-radius': '10px'});
 
-      let figure = $('<figure style="color:transparent;" class="col-lg-4"></figure>');
-      $(figure).css({ 'width': 'auto', 'height': '250px', 'text-align': 'center', 'text-transform': 'capitalize', 'font-weight': 'bold'});
+      let figure = $('<figure style="color:transparent" class="col-lg-4"></figure>');
       figure.append(searchImage[i]);
+      $(figure).css({ 'width': 'auto', 'height': '250px', 'text-align': 'center', 'text-transform': 'capitalize', 'font-weight': 'bold'});
+
       let altImage = $(searchImage[i]).attr('alt');
-      figure.append('<figcaption class="figcaption-image" style="font-size: 30px">' + altImage + '</figcaption>');
+      let figcaption = $('<figcaption>' + altImage + '</figcaption>');
+      figure.append(figcaption);
 
       containerX.append(figure);
       figure.hover(() => {
         $(searchImage[i]).css({'visibility': 'hidden'});
         $(searchImage[i]).animate(4000);
-        $(figure).css({'color': 'black'});
+        $(figure).css({'color': 'black', 'text-shadow': '0px 0px black'});
+        figure.addClass('col-lg-4');
+        figcaption.css({'font-size': '30px', 'transform': 'translateY(-300%)'});
       }, () => {
         $(searchImage[i]).css({'visibility': 'visible'});
         $(searchImage[i]).animate(4000);
-        $(figure).css({'color': 'transparent'});
+        $(figure).css({'color': 'white', 'text-shadow': '2px 2px black'});
+        figcaption.css({'transform': 'translateY(0%)', 'font-size': '20px'});
       });
     }
   };
   pluginImage(containerA);
 });
-
-PluginJQ
