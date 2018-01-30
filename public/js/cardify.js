bfd0a3b4edb35ef4ad132176452494b696657e1d
JessicaMP <jessica95.mp@gmail.com>
2018-01-30 17:43:55 -0500

    Transpilado correcto

--- public/js/cardify.js
+++ public/js/cardify.js
@@ -5,18 +5,19 @@
 
     var containerA = $('.container');
 
-    function pluginImage(containerX) {
-
+    var pluginImage = function pluginImage(containerX) {
       var searchImage = containerX.find('img');
 
-      for(var i = 0 ; i < searchImage.length; i++) {
-        //console.log(searchImage[i]);
+      console.log(searchImage);
+
+      var _loop = function _loop(i) {
+        console.log(searchImage[i]);
         searchImage.css({ 'width': '335px', 'height': '200px', 'border-radius': '10px' });
 
         var figure = $('<figure style="color:transparent" class="col-lg-4"></figure>');
         figure.append(searchImage[i]);
         $(figure).css({ 'width': 'auto', 'height': '250px', 'text-align': 'center', 'text-transform': 'capitalize', 'font-weight': 'bold' });
-        
+
         var altImage = $(searchImage[i]).attr('alt');
         var figcaption = $('<figcaption>' + altImage + '</figcaption>');
         figure.append(figcaption);
@@ -28,14 +29,17 @@
           $(figure).css({ 'color': 'black', 'text-shadow': '0px 0px black' });
           figure.addClass('col-lg-4');
           figcaption.css({ 'font-size': '30px', 'transform': 'translateY(-300%)' });
-        },
-        function () {
+        }, function () {
           $(searchImage[i]).css({ 'visibility': 'visible' });
           $(searchImage[i]).animate(4000);
           $(figure).css({ 'color': 'white', 'text-shadow': '2px 2px black' });
           figcaption.css({ 'transform': 'translateY(0%)', 'font-size': '20px' });
         });
       };
+
+      for (var i = 0; i < searchImage.length; i++) {
+        _loop(i);
+      }
     };
     pluginImage(containerA);
   };
