7f8069262c054539e63097412060bd3acac73aa8
meliveloz <mvelozcampos@gmail.com>
2018-01-30 19:55:51 -0300

    fix test

--- test/test.js
+++ test/test.js
@@ -1,51 +1,61 @@
- var chai = require('chai');
- //chai.use(require('chai-dom'));
-const { should } = require('chai');
-const jsdom = require("jsdom");
+
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-let dom = new JSDOM('<!DOCTYPE html><html><body><div class="container"><img src="imagen.jpg" alt="hola test"></></div></body></html>');
+let dom = new JSDOM('<!DOCTYPE html><html><body><div class="container"><img src="../assets/img/tres.jpg" alt="hola test"></></div></body></html>');
 const { window } = dom;
 global.document = dom;
 global.window = window;
 global.navigator = {
-    userAgent: 'node.js',
+  userAgent: 'node.js',
 }; 
-var $ = require('jquery');
+
+const $ = require('jquery');
+var chai = require('chai');
+var plugin = require('chai-jq');
+// Inject plugin.
+chai.use(plugin);
+
 const cardify = require('../src/app');
 
-describe('body children', function() {
-it('passes cuando el body tiene hijos', function() {
-   dom.window.document.body.children.length >= 1;
-   // quiero saber si existen elementos en el body
-  });
-it('passes cuando existen imagenes dentro de container', function() {
-  $('.container').children('img').length >= 1; 
-})
-it('passes cuando el atributo src no esta vacio ', function() {
- $('img').attr('src') !== '';
-  })
-it('passes cuando el atributo alt no esta vacio', function() {
- $('img').attr('alt') !== '';
-  })
-it("passes cuando la class es igual a container", () => {
-  $('img').class === 'container'
-   })
-it("passes cuando la imagen es png o jpg", () => {
-  $('img').attr('src').substring(('img').lastIndexOf('.')) === ".jpg" && ".png" 
-   });
-});
-  afterEach(function (done) {
-    setTimeout(done, 200);
-});
 describe('cardify', () => {
-  $('.container').cardify;
-  it('Passes si se crea figure', () => {
-    chai.assert.exists($('figure'));
+  $('.container').cardify();
+
+  it('passes si figure no tiene atributo class', () => {
+    chai.expect($('figure')).to.not.have.$data('class');
+  });
+   
+  it('Passes cuando figcaption tiene position absolute', () => {
+    chai.expect($('figcaption')).to.have.$css('position', 'absolute');
+  });
+  it('Passes cuando h3 tiene class text-caption', () => {
+    chai.expect($('h3')).to.have.$class('text-caption');
+  });
+  it('passes cuando el atributo src no esta vacio ', function() {
+    $('img').attr('src') !== '';
+  });
+  it('passes cuando el body tiene hijos', function() {
+    dom.window.document.body.children.length >= 1;
+    // quiero saber si existen elementos en el body
+  });
+  it('passes cuando existen imagenes dentro de container', function() {
+    $('.container').children('img').length >= 1; 
+  });
+
+  it('passes cuando el atributo alt no esta vacio', function() {
+    $('img').attr('alt') !== '';
+  });
+
+  it('passes cuando la imagen es png o jpg', () => {
+    $('img').attr('src').substring(('img').lastIndexOf('.')) === '.jpg' && '.png'; 
+  });
+  it('passes si figure no tiene atributo class', () => {
+    chai.expect($('figure')).to.not.have.$data('class');
+  });
+  it('passes si figure no tiene atributo class', () => {
+    chai.expect($('.text-caption')).to.have.$css('position', 'absolute');
   });
 });
-describe('cardify', () => {
-  $('.container').cardify;
-  it('Passes si crea figcaption', () => {
-    chai.assert.exists($('figcaption'));
-  });
+afterEach(function(done) { //dynamic cat test !
+  setTimeout(done, 200);
 });
+
