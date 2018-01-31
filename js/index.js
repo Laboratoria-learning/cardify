8c62b7940c8827a713d145b5b19d2ce76124e435
Jennifer Carmen <32302079+Jennifercarmen@users.noreply.github.com>
2018-01-31 13:07:41 -0500

    es6

--- js/index.js
+++ js/index.js
@@ -1,37 +1,29 @@
-(function($) {
+(($) => {
   // Declaración del plugin.
+  
   $.fn.pluss = function(options) {
     // Obtenemos los parámetros.
     options = $.extend({}, $.fn.pluss.defaultOptions, options);
-    
-     this.each(function(index) {
-      $('.container').append(
-        '<figure>' +
-            '<figcaption>' + $(this).attr('alt') + '</figcaption>' +
-            '</figure>'
-      );
-      $('figure')[index].prepend($(this)[0]);
-      var element = $('figure');
-      var container=$('.container');
-            container.addClass(options.direction);
-      element.addClass(options.align);
-        
-      $( this ).mouseover(function() {
-        $('.container img').eq(index).addClass('imgdisplay');
-        $('figcaption:eq('+(index)+')').addClass('figcaptionhover');
-        
+    $(this).find('img').each(function(index) {
+      $(this).wrap('<figure></figure>');
+      $(this).parent().append('<figcaption>' + $(this).attr('alt') + '</figcaption>');
+      // Estableciendo clases (horizontal o vertical---por defecto vertical)
+      $(this).parent().parent().addClass(options.direction);
+      $(this).parent().addClass(options.align);
+      //  Hover
+      $(this).mouseover(() => {
+        $(this).addClass('imgdisplay');
+        $('figcaption:eq(' + (index) + ')').addClass('figcaptionhover');
       });
-      $('figcaption:eq('+(index)+')' ).mouseout(function() {
-        $('.container img').eq(index).removeClass('imgdisplay');
-        $('figcaption:eq('+(index)+')').removeClass('figcaptionhover');
-              });
-
+      $('figcaption:eq(' + (index) + ')').mouseout(() => {
+        $('figcaption:eq(' + (index) + ')').prev().removeClass('imgdisplay');
+        $('figcaption:eq(' + (index) + ')').removeClass('figcaptionhover');
+      });
     });
-  };  
-     // Parametros del plugin.
-
-     $.fn.pluss.defaultOptions = {
-      align: 'vertical',
-      direction: ''
-    }
+  };
+  // Parametros del plugin.
+  $.fn.pluss.defaultOptions = {
+    align: 'vertical',
+    direction: ''
+  };
 })(jQuery);
