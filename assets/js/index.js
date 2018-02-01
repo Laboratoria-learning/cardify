5766e00a2745b9642c5d2c01ca45c76b66c9822f
tcltere <t.canette@gmail.com>
2018-02-01 14:48:03 -0300

    function nueva

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,38 @@
 
-$(document).ready(function(){
-   
-    //Al pasar sobre .gepic animamos la imágen dando un tamaño menor y mostramos el div .negro
-    $('.imagenes').mouseenter(function(e) {
-       const alt = $ ('img').attr('alt')
-       console.log(alt)
+$(document).ready(function() {
+  $(".content img").mouseover(function(){
+       
+    let imagenFifure = $(this).attr("alt");
+    $(this).each(function(){
+    $(this).wrap('<figure></figure>')
+     $('figure').css({
+          'display': 'inline-block',
+           'border': '3px solid #eee',
+           'width': '250px',
+           'margin':'30px',
 
-        $(this).children('a').children('img').animate({ height: '240', left: '0', top: '0', width: '310'}, 150);
-        $(this).children('a').children('div').fadeIn(150);
-    $(this).children('a').children('.negro').css({ "padding-top": "80px" , "opacity" : 1 });
-     $ ('#texto').html(alt)
-    }).mouseleave(function(e) {
-        $(this).children('a').children('img').animate({ height: '280', left: '-20', top: '-20', width: '350'}, 150);
-        $(this).children('a').children('div').fadeOut(150);
-    $(this).children('a').children('.negro').css({ "padding-top": "150px" , "opacity" : 0 });
+         })    
    
-    }); 
+       
+     $('figure').append('<figcaption>' + imagenFifure + '</figcaption>')
+    $('figcaption').css({'display':'block',
+                        'width':'90%',
+                       
 
+          
+
+           
+        });
+
+ });
+
+    })
+   $(".content img").mouseout(function(){
+    $('img').unwrap();
+   $('figcaption').empty();
+ }); 
 });
-
+  
 
 
 
