746f7b86c8077a573100315a57a82cc5d11005d9
AriellaRogueN <arrodriguezn@gmail.com>
2018-02-01 20:33:31 -0300

    Modificando html-js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,26 @@
 
-const $ = require('jquery');
 
-$(document).ready(function()
-  $(".content img").mouseover(function(){
-       
-    let imagenFifure = $(this).attr("alt");
-    $(this).each(function(){
-    $(this).wrap('<figure></figure>')
-     $('figure').css({
-          'display': 'inline-block',
-           'border': '3px solid #eee',
-           'width': '250px',
-           'margin':'30px',
+$(document).ready(function() {
+  $('.content img').mouseover(function() {
+    let imagenFigure = $(this).attr('alt');
+    $(this).each(function() {
+      $(this).wrap('<figure></figure>');
+      $('figure').css({
+        'display': 'inline-block',
+        'border': '3px solid #eee',
+        'width': '250px',
+        'margin': '30px',
+      });    
+      $('figure').append('<figcaption>' + imagenFigure + '</figcaption>');
+      $('figcaption').css({'display': 'block',
+        'width': '90%', });
+    });
+  });
 
-         })    
-   
-       
-     $('figure').append('<figcaption>' + imagenFifure + '</figcaption>')
-    $('figcaption').css({'display':'block',
-                        'width':'90%',
-                       
-
-
-          
-
-           
-        });
-
- });
-
-    })
-   $(".content img").mouseout(function(){
+  $('.content img').mouseout(function() {
     $('img').unwrap();
-   $('figcaption').empty();
- }); 
+    $('figcaption').remove();
+  });
 });
-  
-
-
-
-
-
-
-
-
-
-
 
   
