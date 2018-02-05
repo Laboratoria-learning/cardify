72819f406ce34537a7cf1fdfc3310f8f7a5b17aa
Jennifecarmen <jennifercarmens@gmail.com>
2018-02-05 02:04:11 -0500

    añadiendo opcion subir imagenes al contenedor

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,35 @@
 $(document).ready(() => {
-  var btnRestart=$('#reiniciar');
+  var btnRestart = $('#reiniciar');
   let btnStart = $('#start');
-btnRestart.hide();
+  var btnAdd = $('#add');
+
+  var fichero = $('#fichero');
+
+  btnRestart.hide();
+  btnAdd.hide();
+
   btnStart.one('click', () => {
-    $('#start').hide();
-    $('#reiniciar').show();
-    $('.container-cardify1').pluss({ 
+    btnStart.hide();
+    btnRestart.show();
+    btnAdd.show();
+
+    $('.container-cardify1').pluss({
     });
   });
-
+  // cargar imagen en div
+  fichero.on('change', (ev) => {
+    var file = ev.target.files[0];
+    var fr = new FileReader();
+    fr.onload = (ev2) => {
+      $('.container-cardify1').append(
+        '<figure class="horizontal">' +
+        '<img src=' + ev2.target.result + ' alt="descripcion" class="cont-img">' +
+        '<figcaption class="hidden">descripcion1</figcaption>' +
+        '</figure>'
+      );
+    };
+    fr.readAsDataURL(file);
+  });
 
   let btnStartvertical = $('#startvertical');
   btnStartvertical.one('click', () => {
@@ -19,9 +40,9 @@ btnRestart.hide();
   });
 
   let btnstartHorizontal = $('#startHorizontal');
-  
+
   btnstartHorizontal.one('click', () => {
-    $('.container-horizontal').pluss({ 
+    $('.container-horizontal').pluss({
     });
   });
 });
