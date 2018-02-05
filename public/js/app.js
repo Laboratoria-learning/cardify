cc9ab9f618b0081578b54fa7c534dcffd0932403
Jennifecarmen <jennifercarmens@gmail.com>
2018-02-05 01:00:23 -0500

    añadiendo boton reiniciar

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(() => {
+  var btnRestart=$('#reiniciar');
   let btnStart = $('#start');
-
+btnRestart.hide();
   btnStart.one('click', () => {
+    $('#start').hide();
+    $('#reiniciar').show();
     $('.container-cardify1').pluss({ 
-   
     });
   });
+
 
   let btnStartvertical = $('#startvertical');
   btnStartvertical.one('click', () => {
